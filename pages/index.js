e578e707deee99ed0b1ccdeb99effcc95645f2e9
Aadithya H Rao <aadithyarao@gmail.com>
2023-03-18 20:06:40 +0530

    exploring next.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import Image from "next/image";
+import Script from "next/script";
+import Head from "next/head";
 
 function App() {
     const [data, setData] = useState([]);
@@ -16,20 +19,23 @@ function App() {
     }, []);
 
     return (
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {data.map(item => (
-                <div style={{ border: "5px solid black", display: "block", padding: "3vw", width: "20vw" }} key={item._id}>
-                    <ul>
-                        {item.fooditems.map((snack, i) => (
-                            <li style={{ fontSize: "2vw", margin: "10px", marginBottom: "2px" }} key={i}>{snack.name} - ₹{snack.price}</li>
-                        ))}
-                    </ul>
-                    <p style={{ fontSize: "4vh" }}>Total Cost : ₹{item.cost}</p>
-                    <p style={{ fontSize: "3vh" }}>Time Of Order : {item.ordertime}</p>
+        <>
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {data.map(item => (
+                    <div style={{ border: "5px solid black", display: "block", padding: "3vw", width: "20vw" }} key={item._id}>
+                        <ul>
+                            {item.fooditems.map((snack, i) => (
+                                <li style={{ fontSize: "2vw", margin: "10px", marginBottom: "2px" }} key={i}>{snack.name} - ₹{snack.price}</li>
+                            ))}
+                        </ul>
+                        <p style={{ fontSize: "4vh" }}>Total Cost : ₹{item.cost}</p>
+                        <p style={{ fontSize: "3vh" }}>Time Of Order : {item.ordertime}</p>
 
-                </div>
-            ))}
-        </div>
+                    </div>
+                ))}
+            </div>
+            <Image src="/images/test.png" width={500} height={500}></Image>
+        </>
     );
 }
 
---
+++ pages/login/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function FirstPost() {
+    return <>
+    {/* <div style={{backgroundColor:"yellow",height:"1000vh"}}></div> */}
+    <Link href="/ ">Yo</Link>
+    <h1>First Post</h1>
+    </>;
+
+  }
