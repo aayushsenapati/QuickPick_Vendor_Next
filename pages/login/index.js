d5aa72df4c6b512aa8014a96e7da5174d1d607cf
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2023-03-21 21:01:41 +0530

    quickPick_Website

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,10 +1,10 @@
 import Link from "next/link"
+import styles from "/styles/login.module.css"
 
 export default function FirstPost() {
     return <>
     {/* <div style={{backgroundColor:"yellow",height:"1000vh"}}></div> */}
     <Link href="/ ">Yo</Link>
-    <h1>First Post</h1>
+    <h1 className="test">First Post</h1>
     </>;
-
   }
