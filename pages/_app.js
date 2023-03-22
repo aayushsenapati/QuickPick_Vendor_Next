eb8ba84d36e923f29b59198706d0e8bc564c6b84
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2023-03-22 12:23:37 +0530

    halal

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css'
-import { UserProvider } from '@auth0/nextjs-auth0/client'
-
-export default function App({ Component, pageProps }) {
-  return(<UserProvider>
-    <Component {...pageProps} />
-  </UserProvider>)
-}
