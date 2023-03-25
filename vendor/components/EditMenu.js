3e497c785473619904d051cdb0d8bb10d4b958d6
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-03-25 18:07:03 +0530

    asd

--- vendor/components/EditMenu.js
+++ vendor/components/EditMenu.js
@@ -1,5 +1,23 @@
 import { useState } from 'react';
 
 export default function EditMenu() {
- 
+      //form to enter name and price of item
+      const [name, setName] = useState('');
+      const [price, setPrice] = useState('');
+  
+      const handleSubmit = (e) => {
+          e.preventDefault();
+          console.log(`Name : ${name}, Price : ${price}`);
+      };
+      return (
+          <form onSubmit={handleSubmit}>
+              <label htmlFor="name">Enter name of item : </label>
+              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+  
+              <label htmlFor="price">Enter price of item : </label>
+              <input type="number" id="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+  
+              <button type="submit">Add Item</button>
+          </form>
+        );
 }
