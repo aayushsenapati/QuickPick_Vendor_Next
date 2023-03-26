32083c0203bc7bb3a09b4805a67473c4e1142b7a
AnanyaMahishi <ananyamahishi@gmail.com>
2023-03-26 22:52:00 +0530

    commit 1

---
+++ tw/src/App.js
@@ -0,0 +1,11 @@
+import Sidebar from "./sidebar";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
