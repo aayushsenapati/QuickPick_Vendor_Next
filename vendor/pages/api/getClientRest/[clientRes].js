74821890c66f7be69c30cb770b09ebbd9713794c
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2023-03-26 14:13:30 +0530

    ndnnd

--- vendor/pages/api/getClientRest/[clientRes].js
+++ vendor/pages/api/getClientRest/[clientRes].js
@@ -7,11 +7,9 @@ export default async (req, res) => {
         const client = await clientPromise; //query string will look like /api/newClient/clientemail
         const db = client.db('foodOrders')
         const collection = db.collection('menu');
-
         const query = { email: req.query.clientRes };
-
-        //insert new client into db 
-        await collection.findOne(query, { restaurants: 1 },(err,result)=>{if(err)throw err; console.log("Successfully Fetched");res.json()})       
+        const result=await collection.findOne(query, { restaurants: 1 })
+        console.log(result)
     } catch (e) {
         console.error(e);
     }
