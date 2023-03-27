17d0c01e094f637cdf0e26e33f96800c4c19a655
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2023-03-27 11:33:51 +0530

    halal

--- vendor/components/EditMenu.js
+++ vendor/components/EditMenu.js
@@ -1,11 +1,29 @@
 import axios from "axios";
-
+import React, { useState, useEffect } from 'react';
 
 export default function EditMenu(props) {
-      axios.get('/api/getClientRest/'+props.user.email)
-    .then((e)=>{console.log(e)},()=>{console.log("There was an error in api/getClientRest")})
+      const [selectedItem, setSelectedItem] = useState("");
+      const [resArr, setResArr] = useState([]);
+
+      useEffect(() => {
+            async function fetchData() {
+                  const result = await axios.get('/api/getClientRest/' + props.user.email);
+                  setResArr(result.data);
+            }
+            fetchData();
+      }, []);
+
       return (
-            <><div>{props.user.email}</div>
-            <h1>Test</h1></>
+            <div>
+                  <select value={selectedItem} onChange={(e) => setSelectedItem(e.target.value)}>
+                        <option value="">Select an item</option>
+                        {resArr.map((item,i) => (
+                              <option key={i} value={item.name} >
+                                    {item.name}
+                              </option>
+                        ))}
+                  </select>
+                  {selectedItem && <p>You have selected: {selectedItem}</p>}
+            </div>
       )
 }
--- vendor/pages/api/getClientRest/[clientRes].js
+++ vendor/pages/api/getClientRest/[clientRes].js
@@ -9,7 +9,7 @@ export default async (req, res) => {
         const collection = db.collection('menu');
         const query = { email: req.query.clientRes };
         const result=await collection.find(query,{projection:{restaurants:1,_id:0}}).toArray();
-        console.log(result[0].restaurants)
+        res.json(result[0].restaurants)
     } catch (e) {
         console.error(e);
     }
