1d498b6dc6508fba60ffd2ec94bc731c394811b8
AnanyaMahishi <ananyamahishi@gmail.com>
2024-01-21 18:35:01 +0530

    navbar

--- vendor/app/components/Navbar.js
+++ vendor/app/components/Navbar.js
@@ -1,13 +1,27 @@
-//logo
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { NAV_LINKS } from '@/constants';
 
 const Navbar = () => {
-  return (
-    <div>
+  const router = useRouter();
 
-      navbar
-    </div>
-  )
+  return (
+    <nav className='flexBetween max-container padding-container relative z-30 py-5 flex'>
+      <div onClick={() => router.push('/')}>
+        <Image src='/qp_logo_nobg.png' alt="logo" width={110} height={100} className='ml-10'/>
+      </div>
+      <ul className='hidden h-full gap-12 lg:flex ml-auto mr-10'>
+        {NAV_LINKS.map((link) => (
+          <li key={link.key} className='regular-16 text-gray-55 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold' onClick={() => router.push(link.href)}>
+            {link.label}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+
---
+++ vendor/constants/index.js
@@ -0,0 +1,6 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'about', label: 'About' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
