0591bfce8b897390a04994e7ce8968101c84c2bb
AnanyaMahishi <ananyamahishi@gmail.com>
2024-01-22 20:55:59 +0530

    landing 2.0

--- vendor/app/components/About.js
+++ vendor/app/components/About.js
@@ -2,31 +2,43 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const About = () => {
-const router = useRouter();
+  const router = useRouter();
   return (
     <div>
-      
-      <div className="flex flex-col items-start justify-center min-h-screen bg-white-100 pl-32">
-        <h1 className="text-6xl font-bold text-black-500 pl-8">QuickPick</h1>
-        <p className="text-2xl text-black-700 mt-4">Your food, your pick, super quick</p>
-        <div className="flex mt-4 items-center justify-center pl-8 pr-8 pb-8 pt-4">
-            <button
-                onClick={() => router.push('signin')} 
-                className="bg-yellow-400 hover:bg-yellow-700 text-black font-bold py-2 px-8 rounded-full mr-4"
-            >
-                Sign in
-            </button>
-            
-            <button
-                onClick={() => router.push('signup')} 
-                className="bg-white-100 hover:bg-gray-200 text-black font-bold py-2 px-8 rounded-full border border-black"
-            >
-                Sign up
-            </button>
-        </div>
+      <div className='flex flex-row justify-center'>
+              <div className="flex flex-col items-start justify-center  bg-white-100 pl-32">
+                <h1 className="text-6xl font-bold text-black-500 pl-8">QuickPick</h1>
+                <p className="text-2xl text-black-700 mt-4">Your food, your pick, super quick</p>
+                <div className="flex mt-4 items-center justify-center pl-8 pr-8 pb-8 pt-4">
+                  <button
+                    onClick={() => router.push('signin')}
+                    className="bg-yellow-400 hover:bg-yellow-700 text-black font-bold py-2 px-8 rounded-full mr-4"
+                  >
+                    Sign in
+                  </button>
+
+                  <button
+                    onClick={() => router.push('signup')}
+                    className="bg-white-100 hover:bg-gray-200 text-black font-bold py-2 px-8 rounded-full border border-black"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex items-end justify-center pl-64">
+                <Image
+                  src="/landing_img_nobg.png"
+                  alt="landing_img"
+                  width={700}
+                  height={700}
+                  className="mx-auto"
+                />
+              </div>
       </div>
     </div>
   )
--- vendor/app/page.js
+++ vendor/app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     
     <div>
+            
             <Navbar/>
             <About/>
             <Bgimage/>
