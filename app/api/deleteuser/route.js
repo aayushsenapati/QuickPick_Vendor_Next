2577886d522fbdb88c2fa47e00f60fa257a67dd4
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2024-02-09 10:27:29 +0530

    Delete Functionality

--- app/api/deleteuser/route.js
+++ app/api/deleteuser/route.js
@@ -1,7 +1,16 @@
 import { NextResponse } from 'next/server';
-import {auth,db} from '@/app/firebase/config';
+import admin from 'firebase-admin';
 
-
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+  });
+}
 
 export async function DELETE(request) {
   const body = await request.json();
@@ -9,23 +18,23 @@ export async function DELETE(request) {
 
   try {
     // Get user by email
-    const userRecord = await auth.getUserByEmail(email);
+    const userRecord = await admin.auth().getUserByEmail(email);
 
     // Delete user from Firebase Authentication
-    await auth.deleteUser(userRecord.uid);
+    await admin.auth().deleteUser(userRecord.uid);
 
     // Delete user document from 'users' collection
-    await db.collection('users').doc(userRecord.uid).delete();
+    await admin.firestore().collection('users').doc(userRecord.uid).delete();
 
     // Delete user's orders from 'orders' collection
-    const ordersSnapshot = await db.collection('orders').where('user_id', '==', userRecord.uid).get();
+    const ordersSnapshot = await admin.firestore().collection('orders').where('user_id', '==', userRecord.uid).get();
     ordersSnapshot.forEach((doc) => {
-      db.collection('orders').doc(doc.id).delete();
+      admin.firestore().collection('orders').doc(doc.id).delete();
     });
 
     return NextResponse.json({ success: true }, { status: 200 }); // No Content
   } catch (error) {
-    console.error(error);
+    console.error("EERRRRORRRRRRR", error);
     return NextResponse.json({ error: "Failed to delete item" }, { status: 500 });
   }
 }
--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,4 +1,3 @@
-
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -14,7 +13,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ?  getApp() : initializeApp(firebaseConfig);
